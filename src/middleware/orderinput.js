6feb05bd920d34625178bb151dd3045fe6a12afb
bidemi oritunmise <bidoritunmise@gmail.com>
2019-07-15 06:47:26 -0700

    grading

--- src/middleware/orderinput.js
+++ src/middleware/orderinput.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     pricepatchcheck: function (req,res,next){
         console.log(req.body)
-if(typeof req.body.amount == "undefined" ){
+if(typeof req.body.price_offered == "undefined" ){
 	let reply = {
 		"status":409,
 		"error" : "Error!! please fill required input fields "
